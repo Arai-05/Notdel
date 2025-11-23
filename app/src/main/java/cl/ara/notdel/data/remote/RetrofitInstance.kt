2f5eb5c9a05759d62ad844bec5fac4bec53ff18a
Benjamin Aravena <benj.aravenam@duocuc.cl>
2025-11-23 15:31:01 -0300

    Se agrega RetrofitInstance.kt donde se crea la conexion con la api

---
+++ app/src/main/java/cl/ara/notdel/data/remote/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package cl.ara.notdel.data.remote
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://x8ki-letl-twmt.n7.xano.io/api:2nvAVXKA"
+
+    val api: NotebookApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()) //Convierte el json a clases de kotlin
+            .build()
+            .create(NotebookApiService::class.java)
+    }
+}
