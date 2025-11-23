750925fffd0411ccd5b00dd7e63c7e7014b2be75
Benjamin Aravena <benj.aravenam@duocuc.cl>
2025-11-23 13:47:20 -0300

    Se modifica AppDatabase.kt para solo ArriendoDao

--- app/src/main/java/cl/ara/notdel/data/model/AppDatabase.kt
+++ app/src/main/java/cl/ara/notdel/data/model/AppDatabase.kt
@@ -5,21 +5,19 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Notebook::class, Arriendo::class], version = 2, exportSchema = false)
+@Database(entities = [Arriendo::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun arriendoDao(): ArriendoDao
-
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
-
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "notdel_database"
-                ).fallbackToDestructiveMigration().build()
+                ).fallbackToDestructiveMigration().build() // Si se cambia la base de datos, la borra y la crea de nuevo
                 INSTANCE = instance
                 instance
             }
