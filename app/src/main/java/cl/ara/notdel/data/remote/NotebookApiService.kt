58ca63eedfa31117f4fd57490a5b7c570301cdda
Benjamin Aravena <benj.aravenam@duocuc.cl>
2025-12-17 01:05:22 -0300

    Modificacion del apiService para estados

--- app/src/main/java/cl/ara/notdel/data/remote/NotebookApiService.kt
+++ app/src/main/java/cl/ara/notdel/data/remote/NotebookApiService.kt
@@ -2,6 +2,7 @@ package cl.ara.notdel.data.remote
 
 import cl.ara.notdel.data.model.ArriendoRequest
 import cl.ara.notdel.data.model.ArriendoResponse
+import cl.ara.notdel.data.model.EstadoArriendo
 import cl.ara.notdel.data.model.Notebook
 import retrofit2.Response
 import retrofit2.http.Body
@@ -11,6 +12,8 @@ import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
+// Sirve para decirle a Xano que solo se cambiara el estado
+data class EstadoBody(val estado: EstadoArriendo)
 
 interface NotebookApiService {
 
@@ -26,8 +29,8 @@ interface NotebookApiService {
     @PATCH("notebook/{id}")
     suspend fun actualizarDisponibilidad(
         @Path("id") id: Int,
-        @Body body: Map<String, Boolean>
-    ): Response<Void>
+        @Body body: EstadoBody
+    ): Response<Notebook>
 
     //Eliminar Arriendo
     @DELETE("arriendo/{id}")
